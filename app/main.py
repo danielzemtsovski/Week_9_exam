165037895d9c56bec81ecc2d1a4ae21a8da21ff6
Daniel Zemtsovski <d0556789739@gmail.com>
2026-01-29 13:01:06 +0200

    last1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from db_init import init_database
-
+import dal
 app = FastAPI()
 
 init_database()
@@ -11,32 +11,32 @@ def health_check():
 
 @app.get("/q1/customers-credit-limit-outliers")
 def customers_credit_limit_outliers():
-    pass
+    return dal.get_customers_by_credit_limit_range()
 
 @app.get("/q2/orders-null-comments")
 def orders_null_comments():
-    pass
+    return dal.get_orders_with_null_comments()
 
 @app.get("/q3/customers-first-5")
 def customers_first_5():
-    pass
+    return dal.get_first_5_customers()
 
 @app.get("/q4/payments-total-average")
 def payments_total_average():
-    pass
+    return dal.get_payments_total_and_average()
 
 @app.get("/q5/employees-office-phone")
 def employees_office_phone():
-    pass
+    return dal.get_employees_with_office_phone()
 
 @app.get("/q6/customers-shipping-dates")
 def customers_shipping_dates():
-    pass
+    return dal.get_db_connection()
 
 @app.get("/q7/customer-quantity-per-order")
 def customer_quantity_per_order():
-    pass
+    return dal.get_customer_quantity_per_order()
 
 @app.get("/q8/customers-payments-by-lastname-pattern")
 def customers_payments_by_lastname_pattern(pattern: str = "son"):
-    pass
+    return dal.get_customers_payments_by_lastname_pattern()
